9f22548fff4ce910374ad706430473f114c8cc21
Yakut <frendthomas6@gmail.com>
2024-08-15 22:32:05 +0700

    afas
    asf

--- Crypto162/Assets/Normal/Examples/Hoverbird Player/Scripts/RandomMap.cs
+++ Crypto162/Assets/Normal/Examples/Hoverbird Player/Scripts/RandomMap.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class RandomMap : MonoBehaviour
 {
     public GameObject mp;
+    public GameObject[] maps;
     public Material[] mat;
     // Start is called before the first frame update
     void Start()
     {
         int pler = Random.Range(0,mat.Length);
         mp.GetComponent<Renderer>().material = mat[pler];
+        maps[pler].SetActive(true);
     }
 
     // Update is called once per frame
