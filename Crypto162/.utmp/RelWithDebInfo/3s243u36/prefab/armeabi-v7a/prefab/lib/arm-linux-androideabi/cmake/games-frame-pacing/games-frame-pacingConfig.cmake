3d412770dffa9d8b4a5e2278c1dfae987e169e88
Yakut <frendthomas6@gmail.com>
2024-09-29 13:09:29 +0700

    sigma
    dasadssad

---
+++ Crypto162/.utmp/RelWithDebInfo/3s243u36/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/games-frame-pacing/games-frame-pacingConfig.cmake
@@ -0,0 +1,18 @@
+if(NOT TARGET games-frame-pacing::swappy)
+add_library(games-frame-pacing::swappy SHARED IMPORTED)
+set_target_properties(games-frame-pacing::swappy PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Vincent/.gradle/caches/transforms-3/3fb496842553c62dbf8cc8f8058e05c2/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy/libs/android.armeabi-v7a_API22_NDK23_cpp_shared_Release/libswappy.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Vincent/.gradle/caches/transforms-3/3fb496842553c62dbf8cc8f8058e05c2/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET games-frame-pacing::swappy_static)
+add_library(games-frame-pacing::swappy_static STATIC IMPORTED)
+set_target_properties(games-frame-pacing::swappy_static PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Vincent/.gradle/caches/transforms-3/3fb496842553c62dbf8cc8f8058e05c2/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy_static/libs/android.armeabi-v7a_API22_NDK23_cpp_shared_Release/libswappy.a"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Vincent/.gradle/caches/transforms-3/3fb496842553c62dbf8cc8f8058e05c2/transformed/jetified-games-frame-pacing-1.10.0/prefab/modules/swappy_static/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
